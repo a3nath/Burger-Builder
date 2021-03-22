0527c73429153b75187e4f43e0708273bf6f4fa6
a3nath <amarn.sks@gmail.com>
2021-03-22 10:43:55 -0400

    Total

--- src/components/Salad/Salad.js
+++ src/components/Salad/Salad.js
@@ -5,14 +5,14 @@ import SaladIngredient from './SaladIngredient/SaladIngredient';
 
 const salad = ( props ) => {
     let transformedIngredients = Object.keys( props.ingredients )
-        .map( igKey => {
-            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
-                return <SaladIngredient key={igKey + i} type={igKey} />
-            } );
-        } )
-        .reduce((arr, el) => {
-            return arr.concat(el)
-        }, []);
+        // .map( igKey => {
+        //     return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+        //         return <SaladIngredient key={igKey + i} type={igKey} />
+        //     } );
+        // } )
+        // .reduce((arr, el) => {
+        //     return arr.concat(el)
+        // }, []);
     if (transformedIngredients.length === 0) {
         transformedIngredients = <p>Please start adding ingredients!</p>;
     }
--- src/containers/SaladBuilder/SaladBuilder.js
+++ src/containers/SaladBuilder/SaladBuilder.js
@@ -15,32 +15,45 @@ class SaladBuilder extends Component {
             tomato: 0,
             cheese: 2,
             meat: 0
-        }
+        },
+        cost:{
+            lettuce: 1,
+            tomato: 2,
+            cheese: 1,
+            meat: 3
+        },
+        total: 0
     }
 
     addIng = (type) => {
+        console.log(type)
         let newIng = {...this.state.ingredients}
-        newIng[type] += 1
-        this.setState({ingredients: newIng})
+        newIng[type] =+ 1
+        let newTotal = [...this.state.total]
+        newTotal =+ this.state.cost[type]
+
+        this.setState({ingredients: newIng, total: newTotal})
     }
 
     removeIng = (type) => {
         let newIng = {...this.state.ingredients}
-        newIng[type] += 1
-        this.setState({ingredients: newIng})
+        newIng[type] =- 1
+        let newTotal = [...this.state.total]
+        newTotal =- this.state.cost[type]
+
+        this.setState({ingredients: newIng, total:newTotal})
     }
 
     render () {
-
-        const controlArr = Object.keys(this.state.ingredients).map((ing) => {
+        const controlArr = Object.keys(this.state.ingredients).map((ing, key) => {
             return ( 
-                <div key={ing}>
+                <div key={key}>
                     <p>{ing}</p>
-                    <SaladControls addControl={(ing) => this.addIng(ing)} removeControl={() => this.removeIng(ing)}/>
+                    <SaladControls addControl={(ing) => this.addIng(ing)} removeControl={(ing) => this.removeIng(ing)}/>
                 </div>)
         })
 
-        console.log(controlArr)
+        // console.log(controlArr)
 
         return (
             <Aux>
