01f920a555a0967edc18e506bd6cdcafaae75c7e
a3nath <amarn.sks@gmail.com>
2021-03-24 22:47:01 -0400

    Navigation

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './NavigationItem.module.css';
+
+const navigationItem = (props) => (
+    <li className={classes.NavigationItem}>
+        <a 
+            href={props.link} 
+            className={props.active ? classes.active : null}>
+        </a>{props.children}
+    </li>
+)
+
+export default navigationItem;
