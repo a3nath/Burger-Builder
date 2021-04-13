3c283f5ca6f66b9c4b8f695c94fe8ca2dfcd1650
a3nath <amarn.sks@gmail.com>
2021-04-13 17:57:27 -0400

    contact data post

---
+++ src/components/Input/Input.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import classes from './Input.module.css';
+
+const input = (props) => {
+
+    let inputElement = null;
+
+    switch (props.inputType) {
+        case('input'):
+            inputElement = <input 
+                onChange={props.changed}
+                className={classes.inputElement} 
+                type={props.type}/>
+        // case('select'):
+        //     inputElement = 
+        //         <select onChange={props.changed} 
+        //             className={classes.inputElement} value={props.value}>
+        //             {props.options.map(opt => {
+        //                 <option key={opt.value} value={opt.value}>{opt.displayValue}</option>
+        //             })}
+        //         </select> 
+        default:
+            inputElement = <input 
+            onChange={props.changed}
+                className={classes.inputElement} 
+            type={props.type}/>
+    }
+
+    return(
+        <div>
+            <label className={classes.Label}>{props.name}</label>
+            {inputElement}
+        </div>
+    )
+}
+
+export default input;
--- src/containers/CheckoutBuilder/ContactData/ContactData.js
+++ src/containers/CheckoutBuilder/ContactData/ContactData.js
@@ -4,30 +4,77 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/Input/Input';
 
 class ContactData extends Component {
 
     state={
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        contactForm:{
+            name: {
+                value:'',
+                elementType:'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                }
+            },
+            email: {
+                value:'',
+                elementType:'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                }
+            },
+            streetAddress: {
+                value:'',
+                elementType:'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Street Address'
+                }
+            },
+            postalCode: {
+                value:'',
+                elementType:'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Postal Code'
+                }
+            },
+            Delivery:{
+                    value:'',
+                    elementType:'select',
+                    elementConfig: {
+                        options: [
+                            {value: 'aa', displayValue: 'aa'},
+                            {value: 'bb', displayValue: 'bb'}
+                        ]
+                },
+            }
         },
         loading:false
     }
 
+    inputHandler = (event, inputElement) => {
+        const newForm = {...this.state.contactForm};
+        const newInput = {...newForm[inputElement]};
+        newInput.value = event.target.value;
+        newForm[inputElement] = newInput;
+        this.setState({contactForm: newForm});
+    }
+
     orderHandler = (event) => {
+        const customerData = {}
         event.preventDefault();
         this.setState({loading: true})
+        for (let index in this.state.contactForm){
+            customerData[index] = this.state.contactForm[index.value]
+        }
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Aaa',
-                contact: '519519519',
-                country: 'Canada'
-            }
+            customer: this.state.customerData
         }
         axios.post('/orders.json', order)
             .then(response => {
@@ -37,19 +84,35 @@ class ContactData extends Component {
             .catch(error => {
                 this.setState({loading: false}) 
             })
-
-         
     }
 
     render(){
+
+        const formArr = []
+
+        for (let index in this.state.contactForm){
+            console.log(this.state.contactForm[index])
+            formArr.push({id: index, config: this.state.contactForm[index]})
+            console.log(formArr)
+        }
+
+        const formInput = formArr.map(element => {
+            return(
+            <Input 
+                key={element.id} 
+                inputType= {element.config.elementType} 
+                name={element.id} 
+                value ={element.config.value} 
+                placholder={element.config.elementConfig.placeholder} 
+                type={element.config.elementConfig.type}
+                options={element.config.elementConfig.options}
+                changed={(event) => this.inputHandler(event, element.id)}/>)
+        })
+
         let form = (
-            <form>
-                    <input type='text' className={classes.Input} name='name' placeholder='Your Name'/>
-                    <input type='email' name='email' className={classes.Input} placeholder='Your Email'/>
-                    <input type='text' name='street' className={classes.Input} placeholder='Your Address'/>
-                    <input type='text' name='postal' className={classes.Input} placeholder='Your postal code'/>
-                    <Button BtnType='Success' clicked={this.orderHandler}>ORDER</Button>
-                </form>
+            <form onSubmit={this.orderHandler}>
+                {formInput}
+            </form>
         ); 
 
         if (this.state.loading == true){
@@ -60,6 +123,7 @@ class ContactData extends Component {
             <div className={classes.ContactData}>
                 <h4>Enter Contact data</h4>
                 {form}
+                <Button BtnType='Success'>ORDER</Button>
             </div>
         )
     }
