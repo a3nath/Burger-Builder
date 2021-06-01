2e7f1d481da5fa9512e722b39282f43bf8078786
a3nath <amarn.sks@gmail.com>
2021-05-31 21:34:40 -0400

    login form start

--- src/containers/Authentication/Auth.js
+++ src/containers/Authentication/Auth.js
@@ -16,7 +16,7 @@ const AuthData = props => {
             elementType:'input',
             elementConfig: {
                 type: 'email',
-                placeholder: 'Your Email'
+                placeholder: 'Email'
             },
             validation: {
                 required:true
@@ -29,7 +29,7 @@ const AuthData = props => {
             elementType:'input',
             elementConfig: {
                 type: 'password',
-                placeholder: 'Your Password'
+                placeholder: 'Password'
             },
             validation: {
                 required:true,
